1722244b1825cefe31adc623b904abe5420c302a
Yolisa Mzolisa <ypmzolisa@gmail.com>
2024-05-22 16:15:20 +0200

    task complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+10.times do
+  Flat.create(
+    name: Faker::Name.name,
+    address: Faker::Address.full_address,
+    description: Faker::Quote.matz,
+    price_per_night: Faker::Number.decimal(l_digits: 2),
+    number_of_guests: Faker::Number.between(from: 1, to: 10),
+    picture_url: "https://images.unsplash.com/photo-1570129477492-45c003edd2be?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  )
+end
